cd4d13ca88624e2c20ad57c6ee9b45e930924758
Hiroki-Fujishima <ucuc.as@gmail.com>
2020-04-28 00:28:35 +0900

    ポートフォリオ用のサンプル

---
+++ Documents/ポートフォリオ 関連/sample2/hiroki-react-app/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Header from './Header';
+import MyPaper from './MyPaper';
+import MyPanel from './MyPanel';
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <MyPaper/>
+      <MyPanel/>
+
+    </div>
+  );
+}
+
+export default App;
